71400a3b5a3f73c4edd000113fbecbb5c9cb183b
VictorApaez <vic.apaez@gmail.com>
2023-10-20 19:26:27 -0400

    created test enviorment using sqlite db

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,6 +6,11 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
-  test: {},
+  test: {
+    database: "test_database",
+    dialect: "sqlite",
+    storage: `./__tests__/test_database.db`,
+  },
+
   production: {},
 };
