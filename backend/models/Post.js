412b56fb97dab433d58e5a620dd37bf75faa6958
VictorApaez <vic.apaez@gmail.com>
2023-10-20 10:04:57 -0400

    create post model that belongs to a user

---
+++ backend/models/Post.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define(
+    "Post",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+      tableName: "posts",
+    }
+  );
+
+  Post.associate = (models) => {
+    Post.belongsTo(models.User, {
+      foreignKey: "userId",
+      as: "user",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+  };
+
+  return Post;
+};
