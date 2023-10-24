a1144ab10495aee488ba6b583215cbc215f36fc9
VictorApaez <vic.apaez@gmail.com>
2023-10-24 19:12:32 -0400

    removed async function and importing models statically

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,16 +1,14 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 import Sequelize, { DataTypes } from "sequelize";
 import configRaw from "../config/config.js";
+
+import UserModel from "./User.js";
+import PostModel from "./Post.js";
+import CommentModel from "./Comment.js";
 
 const env = process.env.NODE_ENV || "development";
 const config = configRaw[env];
 
 const db = {};
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
-const basename = path.basename(filename);
 
 let sequelize = new Sequelize(
   config.database,
@@ -19,32 +17,17 @@ let sequelize = new Sequelize(
   config
 );
 
-export async function initializeDatabase() {
-  const files = fs
-    .readdirSync(dirname)
-    .filter(
-      (file) =>
-        file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
+db.User = UserModel(sequelize, DataTypes);
+db.Post = PostModel(sequelize, DataTypes);
+db.Comment = CommentModel(sequelize, DataTypes);
 
-  await Promise.all(
-    files.map(async (file) => {
-      const module = await import(path.join(dirname, file));
-      const model = module.default(sequelize, DataTypes);
-      db[model.name] = model;
-    })
-  );
+Object.values(db).forEach((model) => {
+  if (model.associate) {
+    model.associate(db);
+  }
+});
 
-  Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
-      db[modelName].associate(db);
-    }
-  });
-
-  db.sequelize = sequelize;
-  db.Sequelize = Sequelize;
-
-  return db;
-}
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 export default db;
