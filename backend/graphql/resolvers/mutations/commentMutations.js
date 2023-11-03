c652ee3e5bc871fa5b90159fd8463431960c52da
VictorApaez <vic.apaez@gmail.com>
2023-11-03 19:17:12 -0400

    using auth helper function to authenticate user and validating post ids

--- backend/graphql/resolvers/mutations/commentMutations.js
+++ backend/graphql/resolvers/mutations/commentMutations.js
@@ -1,28 +1,56 @@
 import db from "../../../models/index.js";
-const { Comment } = db;
+import authenticateAndAuthorize from "../authHelper.js";
+const { Comment, User, Post } = db;
 
 export default {
   Mutation: {
-    async createComment(_, { postId, userId, parentId, content }) {
+    async createComment(_, { postId, parentId, content }, context) {
+      if (!context.authPayload) {
+        throw new Error("You must be authenticated to create a comment.");
+      }
+
+      const user = await User.findOne({
+        where: { auth0Id: context.authPayload.sub },
+      });
+
+      if (!user) {
+        throw new Error("User not found.");
+      }
+
+      const post = await Post.findOne({
+        where: { id: postId },
+      });
+
+      if (!post) {
+        throw new Error("Post not found");
+      }
+
       const comment = await Comment.create({
         postId,
-        userId,
+        userId: user.id,
         parentId,
         content,
       });
+
       return comment;
     },
 
-    async updateComment(_, { id, content }) {
-      const comment = await Comment.findByPk(id);
-      if (!comment) return null;
+    async updateComment(_, { id, content }, context) {
+      const comment = await authenticateAndAuthorize(
+        context.authPayload,
+        id,
+        "Comment"
+      );
       await comment.update({ content });
       return comment;
     },
 
-    async deleteComment(_, { id }) {
-      const comment = await Comment.findByPk(id);
-      if (!comment) return false;
+    async deleteComment(_, { id }, context) {
+      const comment = await authenticateAndAuthorize(
+        context.authPayload,
+        id,
+        "Comment"
+      );
       await comment.destroy();
       return true;
     },
