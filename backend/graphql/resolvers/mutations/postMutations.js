5c646153bed7282dbc5308f08649f538a4ccf621
VictorApaez <vic.apaez@gmail.com>
2023-11-03 19:18:06 -0400

    using auth helper function to authenticate user for all mutations

--- backend/graphql/resolvers/mutations/postMutations.js
+++ backend/graphql/resolvers/mutations/postMutations.js
@@ -1,23 +1,44 @@
 import db from "../../../models/index.js";
-const { Post } = db;
+import authenticateAndAuthorize from "../authHelper.js";
+const { Post, User } = db;
 
 export default {
   Mutation: {
-    async createPost(_, { userId, content }) {
-      const post = await Post.create({ userId, content });
+    async createPost(_, { content }, context) {
+      if (!context.authPayload) {
+        throw new Error("You must be authenticated to create a post.");
+      }
+
+      const auth0Id = context.authPayload.sub;
+
+      const user = await User.findOne({ where: { auth0Id } });
+      if (!user) {
+        throw new Error("User not found.");
+      }
+
+      const post = await Post.create({
+        userId: user.id,
+        content,
+      });
+
       return post;
     },
-
-    async updatePost(_, { id, content }) {
-      const post = await Post.findByPk(id);
-      if (!post) return null;
+    async updatePost(_, { postId, content }, context) {
+      const post = await authenticateAndAuthorize(
+        context.authPayload,
+        postId,
+        "Post"
+      );
       await post.update({ content });
       return post;
     },
 
-    async deletePost(_, { id }) {
-      const post = await Post.findByPk(id);
-      if (!post) return false;
+    async deletePost(_, { postId }, context) {
+      const post = await authenticateAndAuthorize(
+        context.authPayload,
+        postId,
+        "Post"
+      );
       await post.destroy();
       return true;
     },
