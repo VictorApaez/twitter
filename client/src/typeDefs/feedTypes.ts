2ca7f54fbc9d91f6eb94d2c22d3c604147853a38
VictorApaez <vic.apaez@gmail.com>
2023-11-09 19:21:07 -0500

    created file to store all type defs

---
+++ client/src/typeDefs/feedTypes.ts
@@ -0,0 +1,57 @@
+import React from "react";
+
+export interface ReplyType {
+  author: string;
+  content: string;
+  id: number;
+  timestamp: string;
+  likes: number;
+}
+
+export interface CommentType {
+  author: string;
+  content: string;
+  id: number;
+  timestamp: string;
+  likes: number;
+  replies: ReplyType[];
+}
+
+export interface PostType {
+  author: string;
+  content: string;
+  id: number;
+  timestamp: string;
+  likes: number;
+  comments: CommentType[];
+}
+export interface PostProps {
+  post: PostType;
+}
+
+export interface PostCommentsProps {
+  comments: CommentType[];
+  showComments: boolean;
+  commentInputRef: React.RefObject<HTMLTextAreaElement>;
+}
+
+export interface CommentProps {
+  comment: CommentType;
+}
+
+export interface RepliesProps {
+  replies: ReplyType[];
+}
+
+export interface EngagementMetricsProps {
+  post: PostType;
+  onCommentsClick: () => void;
+}
+
+export interface CreateCommentProps {
+  commentInputRef: React.RefObject<HTMLTextAreaElement>;
+}
+
+export interface PostActionButtonsProps {
+  handleCommentInputFocus: () => void;
+}
