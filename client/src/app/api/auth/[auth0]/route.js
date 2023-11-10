559213e1ea83d033d1969307462bd21827219485
VictorApaez <vic.apaez@gmail.com>
2023-11-09 19:26:05 -0500

    redirecting users to newsfeed after login or logout and created route for signup

--- client/src/app/api/auth/[auth0]/route.js
+++ client/src/app/api/auth/[auth0]/route.js
@@ -1,2 +1,16 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
-export const GET = handleAuth();
+import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
+
+export const GET = handleAuth({
+  login: handleLogin({
+    returnTo: "/feed",
+  }),
+  logout: handleLogout({
+    returnTo: "/feed",
+  }),
+  signup: handleLogin({
+    authorizationParams: {
+      screen_hint: "signup",
+    },
+    returnTo: "/feed",
+  }),
+});
